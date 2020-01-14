6b578d59aac178ca5e7a212b54b6576ac0c35e7e
LaggyLuck <michale241@gmail.com>
2020-01-14 01:49:02 +0100

    added .env

--- app.js
+++ app.js
@@ -3,15 +3,19 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
 const usersRouter = require('./routes/users')
 const postsRouter = require('./routes/posts')
 
 //connecting to database
-mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect("mongodb+srv://admin2:" + process.env.MONGO_PASS + "@test-lvzxm.mongodb.net/test?retryWrites=true&w=majority", 
+{useNewUrlParser: true, useUnifiedTopology: true})
 //checking connection
-const db = mongoose.connection
-db.on('error', (error) => console.error(error))
-db.once('open', () => console.log('Connected to Databse'))
+//const db = mongoose.connection
+//db.on('error', (error) => console.error(error))
+//db.once('open', () => console.log('Connected to Databse'))
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
 
 app.use(express.json())
 
